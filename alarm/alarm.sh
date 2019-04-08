561b2dd8745e97d44418d4019985da2ebb5ba6d4
Titor8115 <henryl8115@gmail.com>
2019-04-08 06:59:32 -0400

    Update alarm.sh

--- alarm/alarm.sh
+++ alarm/alarm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd
-git clone https://github.com/Titor8115/CS311.git ~/Download| at 22:00 || at 23:00 || at 23:30 || at 23:59
-cd
-cd ~/Downloads/alarm
-./alarm
+afplay ~/Downloads/alarm/alarm/sound.mp3 | at 08:00
+afplay ~/Downloads/alarm/alarm/sound.mp3 | at 08:10
+afplay ~/Downloads/alarm/alarm/sound.mp3 | at 08:20
+afplay ~/Downloads/alarm/alarm/sound.mp3 | at 08:30
+
